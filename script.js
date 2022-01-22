8b2df7b78072e102a32084cd473c69b0972e26c6
a.chebykin <a.chebykin@betinvest.com>
2022-01-23 00:43:53 +0200

    rework for prototype

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-fetch('http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
-    .then((response) =>  response.json())
-    .then((data) => {
-        render(data.location, data.current)
-        console.log(data);
-    })
-
-let render = (locaition, weather)=>{
-    document.getElementById("weather").insertAdjacentHTML(
-        "beforeend",
-        `
+function Weather() {
+    this.api = fetch('http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
+    this.init = (this.api)
+        .then((response) =>  response.json())
+        .then((data) => {
+            let locaition = data.location
+            let weather = data.current
+            console.log(locaition.name)
+            document.getElementById("weather").insertAdjacentHTML(
+                "beforeend",
+                `
         <div>Weather</div>
         <div>${locaition.country}</div>
         <div>
@@ -23,6 +23,6 @@ let render = (locaition, weather)=>{
             </div>
             <div>${weather.condition.text}</div>
         </div>
-    `)
+    `)})
 }
 
